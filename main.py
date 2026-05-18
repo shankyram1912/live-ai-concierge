0fed6e541e69c3e4ea1f3458a3f15c27e4fa0dc1
Shanky Ram <shankyram@google.com>
2026-05-18 11:41:13 +0800

    v6.0 Transcription Lang = EN

--- main.py
+++ main.py
@@ -174,8 +174,8 @@ async def websocket_endpoint(
                 )
             )
         ),            
-        "input_audio_transcription": types.AudioTranscriptionConfig(),
-        "output_audio_transcription": types.AudioTranscriptionConfig(),
+        "input_audio_transcription": types.AudioTranscriptionConfig(language_codes=['en-US']),
+        "output_audio_transcription": types.AudioTranscriptionConfig(language_codes=['en-US']),
     }
 
     # 2. Conditionally inject features exclusive to the Vertex AI Live API
